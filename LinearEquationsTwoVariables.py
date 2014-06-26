2778b74a1d6a3c5579faf9846a6df3d5a5615a47
Stirlitz <fajner1@gmail.com>
2014-06-26 15:54:26 -0600

    Can solve linear equations with two non-zero variables in standard form.

--- LinearEquationsTwoVariables.py
+++ LinearEquationsTwoVariables.py
@@ -1,26 +1,65 @@
-# in format ax+by+c=0
+# Peter Fajner
+# 2014-06-26
 
-eq1 = raw_input("Enter first equation: ").lower()
-eq2 = raw_input("Enter second equation: ").lower()
+# Linear equation solver, two variables in standard form.
 
-eq1a, eq1r = eq1.split("x")
-eq1b, eq1c = eq1r.split("y")
+import sys
 
-try:
-    eq1a = int(eq1a)
-except:
-    eq1a = 1
-try:
-    eq1b = int(eq1b)
-except:
-    if eq1b == '-': eq1b = -1
-    else: eq1b = 1
-try:
-    eq1c = int(eq1c)
-except:
-    if eq1c == '-': eq1c = -1
-    else: eq1c = 1
+def LinearEquationsTwoVariables():
 
-print eq1a
-print eq1b
-print eq1c
+    eqs = [[] for i in range(2)] # holds the coefficient values for both equations
+
+    for i in range(2):
+
+        eq = (raw_input("Enter equation #" + str(i+1) + ": ").lower())
+
+        a, r = eq.split("x") # finds x coefficient
+        b, r = r.split("y") # finds y coefficient
+
+        exit = 0 # apparently I can't raise SystemExit from within a try block (which makes sense), so this is a workaround
+
+        try:
+            c, r = r.split("=") # attempts isolate c coefficient from equal sign, if present
+            if int(r) != 0: # if whatever is to the right of equal sign is not zero, equation is not in standard form; exits program
+                print "Equation " + str(i+1) + " not in standard form!"
+                exit = 1
+        except:
+            c = r
+
+        if exit == 1: raise SystemExit
+
+        try:
+            a = float(a) # convert a to a double
+        except:
+            a = 1.0 # if a is not a number (i.e. isn't present; a cannot be negative), make it 1
+        try:
+            b = float(b) # convert b to a double
+        except:
+            if b == '-': b = -1.0 # if b is not a number but is a '-', make it -1
+            else: b = 1.0 # if b is not present, make it 1
+        try:
+            c = float(c) # convert c to a double
+        except:
+            c = 0.0 # if c is not a number, it isn't present, therefore it is 0
+
+        eqs[i].append(a) # add the variables to the main list
+        eqs[i].append(b)
+        eqs[i].append(c)
+
+    if eqs[0][0] / eqs[1][0] == eqs[0][1] / eqs[1][1] == eqs[0][2] / eqs[1][2]: # check for equal lines
+        return "Lines are the same; infinite number of solutions."
+
+    elif eqs[0][0] / eqs[1][0] == eqs[0][1] / eqs[1][1]: # check for parallel lines
+        return "Lines are parallel; no solution."
+
+    y = (eqs[0][0]*eqs[1][2] - eqs[1][0]*eqs[0][2]) / (eqs[1][0]*eqs[0][1] - eqs[0][0]*eqs[1][1]) # determine y
+    x = -1*(eqs[0][1]*y + eqs[0][2])/eqs[0][0] # determine x
+
+    solution = [x,y]
+    return solution
+
+print "\nEquations must be linear, in standard form (equal to zero), and both variables must be non-zero! Example: 3x+4y-2=0"
+
+print LinearEquationsTwoVariables(), "\n"
+
+raw_input("Press any key to exit... ")
